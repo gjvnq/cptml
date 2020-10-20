6cd3ebd611adaaf2eb995c69450200102b247d6d
G. Queiroz <gabrieljvnq@gmail.com>
2020-10-20 13:57:42 -0300

    PeekReader now supports negative peeks

--- rust/src/peek_reader.rs
+++ rust/src/peek_reader.rs
@@ -5,6 +5,7 @@ use core::fmt::Debug;
 
 const READ_BUF_SIZE: usize = 64;
 const PEEK_RESERVE: usize = 4;
+const PEEK_RESERVE_I: isize = PEEK_RESERVE as isize;
 const READ_LIMIT: usize = READ_BUF_SIZE - PEEK_RESERVE;
 
 #[derive(Debug)]
@@ -20,7 +21,7 @@ impl PeekReader {
     pub fn new(reader: Box<dyn ByteReader>) -> Self {
         let mut ans = PeekReader {
             buf: ['\0'; READ_BUF_SIZE],
-            buf_pos: 0,
+            buf_pos: 1,
             eof: false,
             pos: Position::new(),
             src: reader,
@@ -33,11 +34,15 @@ impl PeekReader {
         self.pos
     }
 
-    pub fn peek(&mut self, dist: usize) -> char {
-        if dist > 4 {
-            panic!("PeekReader::peek(n={}), n is limited to 3", dist);
+    pub fn peek(&mut self, dist: isize) -> char {
+        if !(-PEEK_RESERVE_I <= dist && dist <= PEEK_RESERVE_I) {
+            panic!("PeekReader::peek(n={}), n must be between {} and {}", dist, -PEEK_RESERVE_I, PEEK_RESERVE);
         }
-        self.buf[self.buf_pos + dist - 1]
+        let tmp = (self.buf_pos) as isize + dist;
+        if tmp < 0 {
+            panic!("Something went horribly wrong: dist={} buf_pos={} tmp={} {:?}", dist, self.buf_pos, tmp, self);
+        }
+        self.buf[tmp as usize]
     }
 
     pub fn pop(&mut self) -> char {
@@ -54,8 +59,10 @@ impl PeekReader {
         // Copy chars we had reserved for peeking to the begining of the buffer
         if self.pos.byte != 0 {
             let mut i = 0;
-            let mut j = self.buf_pos;
+            let mut j = self.buf_pos - PEEK_RESERVE;
+            println!("j = {}", j);
             while j < self.buf.len() {
+                println!("{} <- {}", self.buf[i], self.buf[j]);
                 self.buf[i] = self.buf[j];
                 i += 1;
                 j += 1;
@@ -63,10 +70,10 @@ impl PeekReader {
         }
 
         let mut i = match self.pos.byte {
-            0 => 0,
-            _ => self.buf.len() - self.buf_pos,
+            0 => PEEK_RESERVE as usize,
+            _ => self.buf.len() - self.buf_pos + PEEK_RESERVE,
         };
-        self.buf_pos = 0;
+        self.buf_pos = PEEK_RESERVE as usize;
         let mut buf: [u8; 4] = [0; 4];
         while i < self.buf.len() {
             // Read a single byte
@@ -97,8 +104,11 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let s = "hello world!§......................................................................§ªº冬";
+        let s = "hello world!§abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ\n1§ªº冬";
         let mut parser = PeekReader::new(Box::new(s.bytes()));
+        assert_eq!('\0', parser.peek(-1));
+        assert_eq!('h', parser.peek(0));
+        assert_eq!('e', parser.peek(1));
         assert_eq!('h', parser.pop());
         assert_eq!('e', parser.pop());
         assert_eq!('l', parser.pop());
@@ -112,18 +122,27 @@ mod tests {
         assert_eq!('d', parser.pop());
         assert_eq!('!', parser.pop());
         assert_eq!('§', parser.pop());
-        for _ in 0..70 {
-            assert_eq!('.', parser.pop());
+        for c in "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars() {
+            assert_eq!(c, parser.pop());
         }
-        assert_eq!('.', parser.peek(0));
+            // println!("{:?}", parser);
+        assert_eq!('\n', parser.pop());
+        assert_eq!('X', parser.peek(-4));
+        assert_eq!('Y', parser.peek(-3));
+        assert_eq!('Z', parser.peek(-2));
+        assert_eq!('\n', parser.peek(-1));
+        assert_eq!('1', parser.peek(0));
         assert_eq!('§', parser.peek(1));
         assert_eq!('ª', parser.peek(2));
         assert_eq!('º', parser.peek(3));
         assert_eq!('冬', parser.peek(4));
+        assert_eq!('1', parser.pop());
         assert_eq!('§', parser.pop());
         assert_eq!('ª', parser.pop());
         assert_eq!('º', parser.pop());
         assert_eq!('冬', parser.pop());
+        assert_eq!('\0', parser.pop());
+        assert_eq!('\0', parser.pop());
         assert_eq!('\0', parser.pop());
     }
 }
