6cfa564ad2a147eff2dd6f72e63bd5a3d20d6571
G. Queiroz <gabrieljvnq@gmail.com>
2023-01-26 16:36:42 -0300

    larlpop example working

---
+++ rust/src/lib.rs
@@ -0,0 +1,5 @@
+
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
+
---
+++ rust/tests/basics.rs
@@ -0,0 +1,9 @@
+use cptml::calculator1;
+
+#[test]
+fn calculator1() {
+    assert!(calculator1::TermParser::new().parse("22").is_ok());
+    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
+    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
+    assert!(calculator1::TermParser::new().parse("((22)").is_err());
+}
