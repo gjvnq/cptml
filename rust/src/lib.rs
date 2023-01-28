f29142c4293eed5daa4878c6018511e56ba82907
G. Queiroz <gabrieljvnq@gmail.com>
2023-01-28 15:50:11 -0300

    chore: remove larlpop dependency

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,2 @@
-#[macro_use] extern crate lalrpop_util;
-
 pub mod prelude;
 pub mod lexer;
-
-lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
--- rust/tests/basics.rs
+++
@@ -1,9 +0,0 @@
-use cptml::calculator1;
-
-#[test]
-fn calculator1() {
-    assert!(calculator1::TermParser::new().parse("22").is_ok());
-    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
-    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
-    assert!(calculator1::TermParser::new().parse("((22)").is_err());
-}
