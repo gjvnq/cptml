1cde2683361bc1a8af5469521499cc64cc904077
G. Queiroz <gabrieljvnq@gmail.com>
2023-01-29 01:38:32 -0300

    chore: simplify imports and remove warnings

--- rust/src/ast.rs
+++ rust/src/ast.rs
@@ -1,14 +1,15 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete;
-use nom::character::complete::{alphanumeric1, char, multispace0, satisfy};
-use nom::combinator::{map, peek, recognize};
+use nom::character::complete::{char, multispace0};
+use nom::combinator::{map, recognize};
+use nom::error::Error as NomError;
+use nom::error::ErrorKind::{Alpha, Eof};
 use nom::multi::many0;
 use nom::sequence::{pair, separated_pair};
+use nom::Err::Error as NomErr;
 use nom::IResult;
 use unicode_xid::UnicodeXID;
-
-// Todo: implement encoding based on those types
 
 #[derive(Debug, Clone, PartialEq, Default)]
 pub struct IdFullName<'a> {
@@ -35,28 +36,22 @@ fn valid_xid_continue(ch: char) -> bool {
 }
 
 // Parses <XID_START> <XID_CONTINUE>*
-pub fn xid_name(input: &str) -> nom::IResult<&str, &str> {
+pub fn xid_name(input: &str) -> IResult<&str, &str> {
     let mut input_chars = input.char_indices();
 
     match input_chars.next() {
         Some((_, ch)) => {
             if !valid_xid_start(ch) {
                 // Using ErrorKind::Alpha is a bit wrong but it's good enough for now. Unfortunately there's no ErrorKind::XidStart
-                return Err(nom::Err::Error(nom::error::Error::new(
-                    input,
-                    nom::error::ErrorKind::Alpha,
-                )));
+                return Err(NomErr(NomError::new(input, Alpha)));
             }
         }
         None => {
-            return Err(nom::Err::Error(nom::error::Error::new(
-                input,
-                nom::error::ErrorKind::Eof,
-            )));
+            return Err(NomErr(NomError::new(input, Eof)));
         }
     }
 
-    let mut last_pos = 0;
+    let last_pos;
     loop {
         match input_chars.next() {
             Some((pos, ch)) => {
@@ -76,21 +71,21 @@ pub fn xid_name(input: &str) -> nom::IResult<&str, &str> {
 }
 
 // E.g. "!id", "!cptml"
-pub fn idfullname_special(input: &str) -> nom::IResult<&str, (&str, &str)> {
+pub fn idfullname_special(input: &str) -> IResult<&str, (&str, &str)> {
     pair(recognize(char('!')), xid_name)(input)
 }
 
 // E.g. "namespace:name", "namespace:color"
-pub fn idfullname_regular(input: &str) -> nom::IResult<&str, (&str, &str)> {
+pub fn idfullname_regular(input: &str) -> IResult<&str, (&str, &str)> {
     separated_pair(xid_name, char(':'), xid_name)(input)
 }
 
 // E.g. "name", "color"
-pub fn idfullname_local(input: &str) -> nom::IResult<&str, (&str, &str)> {
+pub fn idfullname_local(input: &str) -> IResult<&str, (&str, &str)> {
     map(xid_name, |s: &str| ("", s))(input)
 }
 
-pub fn idfullname(input: &str) -> nom::IResult<&str, IdFullName> {
+pub fn idfullname(input: &str) -> IResult<&str, IdFullName> {
     let (input, (namespace, localname)) =
         alt((idfullname_special, idfullname_regular, idfullname_local))(input)?;
     Ok((
@@ -116,23 +111,23 @@ impl TagAttrValue {
     }
 }
 
-pub fn parse_bool_true(input: &str) -> nom::IResult<&str, bool> {
+pub fn parse_bool_true(input: &str) -> IResult<&str, bool> {
     let (input, _) = tag("true")(input)?;
     Ok((input, true))
 }
 
-pub fn parse_bool_false(input: &str) -> nom::IResult<&str, bool> {
+pub fn parse_bool_false(input: &str) -> IResult<&str, bool> {
     let (input, _) = tag("false")(input)?;
     Ok((input, false))
 }
 
-pub fn tag_args_bool(input: &str) -> nom::IResult<&str, TagAttrValue> {
+pub fn tag_args_bool(input: &str) -> IResult<&str, TagAttrValue> {
     let (input, flag) = alt((parse_bool_true, parse_bool_false))(input)?;
     Ok((input, TagAttrValue::Boolean(flag)))
 }
 
 // TODO: support underscores as thousands separator
-pub fn integer_hex(input: &str) -> nom::IResult<&str, i64> {
+pub fn integer_hex(input: &str) -> IResult<&str, i64> {
     let (input, _) = tag("0x")(input)?;
     let (input, val) = complete::hex_digit1(input)?;
     Ok((
@@ -142,19 +137,19 @@ pub fn integer_hex(input: &str) -> nom::IResult<&str, i64> {
 }
 
 // TODO: support underscores as thousands separator
-pub fn integer_dec(input: &str) -> nom::IResult<&str, i64> {
+pub fn integer_dec(input: &str) -> IResult<&str, i64> {
     let (input, val) = complete::i64(input)?;
     Ok((input, val))
 }
 
-pub fn tag_args_integer(input: &str) -> nom::IResult<&str, TagAttrValue> {
+pub fn tag_args_integer(input: &str) -> IResult<&str, TagAttrValue> {
     let (input, val) = alt((integer_hex, integer_dec))(input)?;
     Ok((input, TagAttrValue::Integer(val)))
 }
 
 pub fn tag_args_pair<'a>(
     input: &'a str,
-) -> nom::IResult<&'a str, (&'a str, IdFullName<'a>, TagAttrValue)> {
+) -> IResult<&'a str, (&'a str, IdFullName<'a>, TagAttrValue)> {
     let (input, whitespace) = multispace0(input)?;
     let (input, name) = idfullname(input)?;
     let (input, _) = char('=')(input)?;
@@ -176,7 +171,7 @@ impl<'a> CurlyTagStart<'a> {
     }
 }
 
-pub fn curly_tag_start<'a>(input: &'a str) -> nom::IResult<&'a str, CurlyTagStart<'a>> {
+pub fn curly_tag_start<'a>(input: &'a str) -> IResult<&'a str, CurlyTagStart<'a>> {
     let (input, _) = recognize(char('{'))(input)?;
     let (input, element) = idfullname(input)?;
     let (input, args) = many0(tag_args_pair)(input)?;
@@ -207,7 +202,7 @@ impl<'a> PointyTagStart<'a> {
     }
 }
 
-pub fn pointy_tag_start<'a>(input: &'a str) -> nom::IResult<&'a str, PointyTagStart<'a>> {
+pub fn pointy_tag_start<'a>(_input: &'a str) -> IResult<&'a str, PointyTagStart<'a>> {
     todo!()
 }
 
@@ -224,7 +219,7 @@ impl<'a> PointyTagEnd<'a> {
     }
 }
 
-pub fn pointy_tag_end<'a>(input: &'a str) -> nom::IResult<&'a str, PointyTagEnd<'a>> {
+pub fn pointy_tag_end<'a>(_input: &'a str) -> IResult<&'a str, PointyTagEnd<'a>> {
     todo!()
 }
 
@@ -240,7 +235,7 @@ impl<'a> InlineText<'a> {
     }
 }
 
-pub fn inline_text<'a>(input: &'a str) -> nom::IResult<&'a str, InlineText<'a>> {
+pub fn inline_text<'a>(_input: &'a str) -> IResult<&'a str, InlineText<'a>> {
     todo!()
 }
 
@@ -255,7 +250,7 @@ impl<'a> Comment<'a> {
     }
 }
 
-pub fn comment<'a>(input: &'a str) -> nom::IResult<&'a str, Comment<'a>> {
+pub fn comment<'a>(_input: &'a str) -> IResult<&'a str, Comment<'a>> {
     todo!()
 }
 
@@ -272,7 +267,7 @@ impl<'a> CodeBlock<'a> {
     }
 }
 
-pub fn codeblock<'a>(input: &'a str) -> nom::IResult<&'a str, CodeBlock<'a>> {
+pub fn codeblock<'a>(_input: &'a str) -> IResult<&'a str, CodeBlock<'a>> {
     todo!()
 }
 
@@ -289,7 +284,7 @@ impl<'a> TexCode<'a> {
     }
 }
 
-pub fn tex_code<'a>(input: &'a str) -> nom::IResult<&'a str, TexCode<'a>> {
+pub fn tex_code<'a>(_input: &'a str) -> IResult<&'a str, TexCode<'a>> {
     todo!()
 }
 
@@ -297,7 +292,6 @@ pub fn tex_code<'a>(input: &'a str) -> nom::IResult<&'a str, TexCode<'a>> {
 mod tests {
     use crate::ast::*;
     use nom::error::ErrorKind::{Alpha, Char, Digit, Eof, Tag};
-    use nom::Err::Error as NomError;
 
     #[test]
     fn test_idfullname_encode_cptml_2() {
@@ -351,14 +345,14 @@ mod tests {
     fn test_curly_tag_start() {
         assert_eq!(
             curly_tag_start(""),
-            Err(nom::Err::Error(nom::error::Error {
+            Err(NomErr(nom::error::Error {
                 input: "",
                 code: Char
             }))
         );
         assert_eq!(
             curly_tag_start("{"),
-            Err(nom::Err::Error(nom::error::Error {
+            Err(NomErr(nom::error::Error {
                 input: "",
                 code: Eof
             }))
@@ -442,7 +436,7 @@ mod tests {
     fn test_tag_integer() {
         assert_eq!(
             tag_args_integer(""),
-            Err(nom::Err::Error(nom::error::Error {
+            Err(NomErr(nom::error::Error {
                 input: "",
                 code: Digit
             }))
@@ -463,7 +457,7 @@ mod tests {
     fn test_tag_bool() {
         assert_eq!(
             tag_args_bool(""),
-            Err(nom::Err::Error(nom::error::Error {
+            Err(NomErr(nom::error::Error {
                 input: "",
                 code: Tag
             }))
@@ -475,14 +469,14 @@ mod tests {
         );
         assert_eq!(
             tag_args_bool("t"),
-            Err(nom::Err::Error(nom::error::Error {
+            Err(NomErr(nom::error::Error {
                 input: "t",
                 code: Tag
             }))
         );
         assert_eq!(
             tag_args_bool("f"),
-            Err(nom::Err::Error(nom::error::Error {
+            Err(NomErr(nom::error::Error {
                 input: "f",
                 code: Tag
             }))
@@ -493,21 +487,21 @@ mod tests {
     fn test_xid_name() {
         assert_eq!(
             xid_name(""),
-            Err(nom::Err::Error(nom::error::Error {
+            Err(NomErr(nom::error::Error {
                 input: "",
                 code: Eof
             }))
         );
         assert_eq!(
             xid_name("_"),
-            Err(nom::Err::Error(nom::error::Error {
+            Err(NomErr(nom::error::Error {
                 input: "_",
                 code: Alpha
             }))
         );
         assert_eq!(
             xid_name("-"),
-            Err(nom::Err::Error(nom::error::Error {
+            Err(NomErr(nom::error::Error {
                 input: "-",
                 code: Alpha
             }))
@@ -525,7 +519,7 @@ mod tests {
         assert_eq!(idfullname_special("!名前"), Ok(("", ("!", "名前"))));
         assert_eq!(
             idfullname_special("cptml"),
-            Err(nom::Err::Error(nom::error::Error {
+            Err(NomErr(nom::error::Error {
                 input: "cptml",
                 code: Char
             }))
@@ -538,7 +532,7 @@ mod tests {
         assert_eq!(idfullname_regular("ns3:名前"), Ok(("", ("ns3", "名前"))));
         assert_eq!(
             idfullname_regular("img"),
-            Err(nom::Err::Error(nom::error::Error {
+            Err(NomErr(nom::error::Error {
                 input: "",
                 code: Char
             }))
